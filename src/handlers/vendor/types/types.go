0c6168e935fa02ce23341f24e3b3c9f001894710
Mehdi Parhizi <mehdi_parhizi7@yahoo.com>
2018-05-13 00:02:24 +0430

    gofmt

--- src/handlers/vendor/types/types.go
+++ src/handlers/vendor/types/types.go
@@ -2,9 +2,9 @@ package types
 
 // Struct containing device information for marshalling/unmarshalling.
 type Device struct {
-    ID          string  `json:"id"`
-    DeviceModel string  `json:"deviceModel"`
-    Name        string  `json:"name"`
-    Note        string  `json:"note"`
-    Serial      string  `json:"serial"`
+	ID          string `json:"id"`
+	DeviceModel string `json:"deviceModel"`
+	Name        string `json:"name"`
+	Note        string `json:"note"`
+	Serial      string `json:"serial"`
 }
