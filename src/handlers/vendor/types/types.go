f72c8cf4445eb91dcb4658ffc70d7455a038a519
Mehdi Parhizi <mehdi_parhizi7@yahoo.com>
2018-05-12 22:22:31 +0430

    Struct type deatached.

---
+++ src/handlers/vendor/types/types.go
@@ -0,0 +1,10 @@
+package types
+
+// Struct containing device information for marshalling/unmarshalling.
+type Device struct {
+    ID          string  `json:"id"`
+    DeviceModel string  `json:"deviceModel"`
+    Name        string  `json:"name"`
+    Note        string  `json:"note"`
+    Serial      string  `json:"serial"`
+}
